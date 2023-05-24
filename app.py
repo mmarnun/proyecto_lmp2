665ad86c3368c31725de7fda1949af618ae0ce32
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-05-24 22:33:43 +0200

    Funcionamiento

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def buscar():
         tipo = request.form.get('tipo')
 
         if not query:
-            return render_template('error.html', message="No se proporcionó un término de búsqueda válido.")
+            return render_template('error.html', mensaje="No se proporcionó un término de búsqueda válido.")
 
         if tipo == "artista":
             parametros_bio = {
@@ -27,26 +27,26 @@ def buscar():
                 "api_key": api_key,
                 "format": "json"
             }
-
             response_bio = requests.get(url_base, params=parametros_bio)
-
             if response_bio.status_code == 200:
                 data_bio = json.loads(response_bio.text)
-                bio = data_bio["artist"]["bio"]["content"]
-
-                parametros_top = {
-                    "method": "artist.gettoptracks",
-                    "artist": query,
-                    "api_key": api_key,
-                    "format": "json",
-                    "limit": 5
-                }
-                response_top5 = requests.get(url_base, params=parametros_top)
-
-                if response_top5.status_code == 200:
-                    data_top = json.loads(response_top5.text)
-                    top_tracks = data_top["toptracks"]["track"]
-                    return render_template('info.html', artista=query, bio=bio, top_tracks=top_tracks)
+                if "artist" in data_bio:
+                    bio = data_bio["artist"]["bio"]["content"]
+                    parametros_top = {
+                        "method": "artist.gettoptracks",
+                        "artist": query,
+                        "api_key": api_key,
+                        "format": "json",
+                        "limit": 5
+                    }
+                    response_top5 = requests.get(url_base, params=parametros_top)
+                    if response_top5.status_code == 200:
+                        data_top = json.loads(response_top5.text)
+                        top_tracks = data_top["toptracks"]["track"]
+                        return render_template('info.html', artista=query, bio=bio, top_tracks=top_tracks)
+                return render_template('error.html', mensaje="No se encontró ningún resultado.")
+            else:
+                return render_template('error.html', mensaje="No se encontró ningún resultado.")
         elif tipo == "tag":
             parametros_tag = {
                 "method": "tag.getinfo",
@@ -54,30 +54,30 @@ def buscar():
                 "api_key": api_key,
                 "format": "json"
             }
-
             response_tag = requests.get(url_base, params=parametros_tag)
-
             if response_tag.status_code == 200:
                 data_tag = json.loads(response_tag.text)
                 tag_info = data_tag["tag"]
                 return render_template('tag.html', tag_info=tag_info)
+            else:
+                return render_template('error.html', mensaje="No se encontró ningún resultado.")
         elif tipo == "cadena":
             parametros_top50 = {
                 "method": "chart.gettopartists",
                 "api_key": api_key,
                 "format": "json",
-                "limit": 50
             }
-
             response_top50 = requests.get(url_base, params=parametros_top50)
-
             if response_top50.status_code == 200:
                 data_top50 = json.loads(response_top50.text)
                 artists = data_top50["artists"]["artist"]
-                filtered_artists = [artist for artist in artists if query.lower() in artist["name"].lower()]
-                return render_template('todo.html', artists=filtered_artists)
+                filtro = [artist for artist in artists if query.lower() in artist["name"].lower()]
+                if filtro:
+                    return render_template('todo.html', artists=filtro)
+                else:
+                    return render_template('error.html', mensaje="No se encontró ningún resultado.")
         else:
-            return render_template('error.html', message="Opción de búsqueda inválida.")
+            return render_template('error.html', mensaje="Opción de búsqueda inválida.")
     else:
         return render_template("buscador.html")
 
@@ -86,15 +86,13 @@ def todo():
     parametros = {
         "method": "chart.gettopartists",
         "api_key": api_key,
-        "format": "json"
+        "format": "json",
     }
-
     response = requests.get(url_base, params=parametros)
-
     if response.status_code == 200:
         data = json.loads(response.text)
         if "error" in data:
-            return render_template('error.html', message=data["message"])
+            return render_template('error.html', mensaje=data["mensaje"])
         else:
             artists = data["artists"]["artist"]
             return render_template('todo.html', artists=artists)
