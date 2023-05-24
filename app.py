6eb90e8129e29f031d1e928c500c08b0746f6822
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-05-24 19:34:35 +0200

    Busqueda por cadena, genero musical...

--- app.py
+++ app.py
@@ -32,58 +32,55 @@ def buscar():
 
             if response_bio.status_code == 200:
                 data_bio = json.loads(response_bio.text)
-                if "error" in data_bio:
-                    return render_template('error.html', message=data_bio["message"])
-                else:
-                    bio = data_bio["artist"]["bio"]["content"]
+                bio = data_bio["artist"]["bio"]["content"]
 
-                    parametros_top = {
-                        "method": "artist.gettoptracks",
-                        "artist": query,
-                        "api_key": api_key,
-                        "format": "json",
-                        "limit": 5
-                    }
+                parametros_top = {
+                    "method": "artist.gettoptracks",
+                    "artist": query,
+                    "api_key": api_key,
+                    "format": "json",
+                    "limit": 5
+                }
+                response_top5 = requests.get(url_base, params=parametros_top)
 
-                    response_top5 = requests.get(url_base, params=parametros_top)
-
-                    if response_top5.status_code == 200:
-                        data_top = json.loads(response_top5.text)
-                        if "error" in data_top:
-                            return render_template('error.html', message=data_top["message"])
-                        else:
-                            top_tracks = data_top["toptracks"]["track"]
-                            return render_template('info.html', artista=query, bio=bio, top_tracks=top_tracks)
-                    else:
-                        return render_template('error.html', message="Ha ocurrido un error al obtener el top de canciones.")
-            else:
-                return render_template('error.html', message="Ha ocurrido un error al realizar la petición de la biografía.")
-        elif tipo == "album":
-            parametros_album = {
-                "method": "album.getinfo",
-                "artist": query,
-                "album": query,
+                if response_top5.status_code == 200:
+                    data_top = json.loads(response_top5.text)
+                    top_tracks = data_top["toptracks"]["track"]
+                    return render_template('info.html', artista=query, bio=bio, top_tracks=top_tracks)
+        elif tipo == "tag":
+            parametros_tag = {
+                "method": "tag.getinfo",
+                "tag": query,
                 "api_key": api_key,
                 "format": "json"
             }
 
-            response_album = requests.get(url_base, params=parametros_album)
+            response_tag = requests.get(url_base, params=parametros_tag)
 
-            if response_album.status_code == 200:
-                data_album = json.loads(response_album.text)
-                if "error" in data_album:
-                    return render_template('error.html', message=data_album["message"])
-                else:
-                    album = data_album["album"]["name"]
-                    tracks = data_album["album"]["tracks"]["track"]
-                    return render_template('album.html', album=album, tracks=tracks)
-            else:
-                return render_template('error.html', message="Ha ocurrido un error al realizar la petición del álbum.")
+            if response_tag.status_code == 200:
+                data_tag = json.loads(response_tag.text)
+                tag_info = data_tag["tag"]
+                return render_template('tag.html', tag_info=tag_info)
+        elif tipo == "cadena":
+            parametros_top50 = {
+                "method": "chart.gettopartists",
+                "api_key": api_key,
+                "format": "json",
+                "limit": 50
+            }
+
+            response_top50 = requests.get(url_base, params=parametros_top50)
+
+            if response_top50.status_code == 200:
+                data_top50 = json.loads(response_top50.text)
+                artists = data_top50["artists"]["artist"]
+                filtered_artists = [artist for artist in artists if query.lower() in artist["name"].lower()]
+                return render_template('todo.html', artists=filtered_artists)
         else:
             return render_template('error.html', message="Opción de búsqueda inválida.")
     else:
         return render_template("buscador.html")
-    
+
 @app.route('/todo')
 def todo():
     parametros = {
@@ -101,8 +98,6 @@ def todo():
         else:
             artists = data["artists"]["artist"]
             return render_template('todo.html', artists=artists)
-    else:
-        return render_template('error.html', message="Ha ocurrido un error al obtener la lista de artistas.")
 
 @app.route('/artista/<nombre>')
 def obtener_artista(nombre):
@@ -115,29 +110,18 @@ def obtener_artista(nombre):
     response_bio = requests.get(url_base, params=parametros_bio)
     if response_bio.status_code == 200:
         data_bio = json.loads(response_bio.text)
-        if "error" in data_bio:
-            return render_template('error.html', message=data_bio["message"])
-        else:
-            bio = data_bio["artist"]["bio"]["content"]
-            parametros_top = {
-                "method": "artist.gettoptracks",
-                "artist": nombre,
-                "api_key": api_key,
-                "format": "json",
-                "limit": 5
-            }
-            response_top5 = requests.get(url_base, params=parametros_top)
-            if response_top5.status_code == 200:
-                data_top = json.loads(response_top5.text)
-                if "error" in data_top:
-                    return render_template('error.html', message=data_top["message"])
-                else:
-                    top_tracks = data_top["toptracks"]["track"]
-                    return render_template('info.html', artista=nombre, bio=bio, top_tracks=top_tracks)
-            else:
-                return render_template('error.html', message="Ha ocurrido un error al obtener el top de canciones.")
-    else:
-        return render_template('error.html', message="Ha ocurrido un error al realizar la petición de la biografía.")
-
+        bio = data_bio["artist"]["bio"]["content"]
+        parametros_top = {
+            "method": "artist.gettoptracks",
+            "artist": nombre,
+            "api_key": api_key,
+            "format": "json",
+            "limit": 5
+        }
+        response_top5 = requests.get(url_base, params=parametros_top)
+        if response_top5.status_code == 200:
+            data_top = json.loads(response_top5.text)
+            top_tracks = data_top["toptracks"]["track"]
+            return render_template('info.html', artista=nombre, bio=bio, top_tracks=top_tracks)
 
 app.run("0.0.0.0", 5000, debug=True)
